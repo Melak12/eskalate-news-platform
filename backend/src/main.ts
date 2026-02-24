7ada8d16b1df1028ea0e35e1d28b5738999fe13a
Melake Wubshet <wubshetmelake@gmail.com>
2026-02-24 20:51:27 +0300

    refactor: move global exception filter and response interceptor to common directory

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ResponseInterceptor } from './core/interceptors/response.interceptor';
-import { GlobalExceptionFilter } from './core/filters/global-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
